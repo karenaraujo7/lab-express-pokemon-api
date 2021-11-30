35d85a9ac512123117c913c73ea85a1d617712f7
Karen <karen_araujp7@hotmail.com>
2021-11-30 01:53:56 -0300

    rota pelo nome

--- pokemons.js
+++ pokemons.js
@@ -15,5 +15,12 @@ pokemonRouter.get("/pokemon/:id", (req, res) => {
     return res.status(200).json({...pokemon});
 });
 
+// Rota que busca pelo nome do pokemon
+pokemonRouter.get("/search/:name", (req, res) => {
+    const pokemon = allPokemon.find((currentPokemon) => {
+        return currentPokemon.name == req.params.name;
+    });
+    return res.status(200).json({...pokemon});
+})
 
 module.exports = pokemonRouter;
